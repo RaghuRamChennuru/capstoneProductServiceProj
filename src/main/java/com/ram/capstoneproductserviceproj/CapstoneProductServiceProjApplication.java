3468c6d63d693d95d6f008b53fa90ede66448d97
unknown <raghuram6857@gmail.com>
2024-01-14 00:30:30 +0530

    Initial commit

---
+++ src/main/java/com/ram/capstoneproductserviceproj/CapstoneProductServiceProjApplication.java
@@ -0,0 +1,13 @@
+package com.ram.capstoneproductserviceproj;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CapstoneProductServiceProjApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CapstoneProductServiceProjApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ram/capstoneproductserviceproj/CapstoneProductServiceProjApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ram.capstoneproductserviceproj;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CapstoneProductServiceProjApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
