cdcaca0af84146bd660e8f34230c97fcc6ef9465
unknown <raghuram6857@gmail.com>
2024-02-14 23:15:00 +0530

    updated inheritance

--- src/main/java/com/ram/capstoneproductserviceproj/Inheritance/JoinedTableClass/mentor.java
+++ src/main/java/com/ram/capstoneproductserviceproj/Inheritance/JoinedTableClass/mentor.java
@@ -2,6 +2,7 @@ package com.ram.capstoneproductserviceproj.Inheritance.JoinedTableClass;
 
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
 import jakarta.persistence.PrimaryKeyJoinColumn;
 import lombok.Getter;
 import lombok.Setter;
@@ -9,10 +10,10 @@ import lombok.Setter;
 @Getter
 @Setter
 @Entity(name = "jt_mentor")
-@PrimaryKeyJoinColumn(name = "user_id")
 public class mentor extends user
 {
-    @Id
-    private long id;
+    //   @JoinColumn(name = "user_id",referencedColumnName = "id")
+   // @Id
+    //private long id;
     private int qualification;
 }
--- src/main/java/com/ram/capstoneproductserviceproj/Inheritance/JoinedTableClass/teachingAssistant.java
+++ src/main/java/com/ram/capstoneproductserviceproj/Inheritance/JoinedTableClass/teachingAssistant.java
@@ -2,6 +2,7 @@ package com.ram.capstoneproductserviceproj.Inheritance.JoinedTableClass;
 
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
 import jakarta.persistence.PrimaryKeyJoinColumn;
 import lombok.Getter;
 import lombok.Setter;
@@ -9,10 +10,12 @@ import lombok.Setter;
 @Getter
 @Setter
 @Entity(name = "jt_ta")
-@PrimaryKeyJoinColumn(name = "user_id")
+
 public class teachingAssistant extends user
 {
-    @Id
-    private long id;
+
+   // @Id
+  //  private long ta_id;
+    //@JoinColumn(name = "user_id",referencedColumnName = "id")
     private int rating;
 }
